f04f501b02297de2b6100b544ee43438f919362e
Walter Gomez <47392003+ictus4u@users.noreply.github.com>
2021-03-30 17:10:11 -0400

    Remove ssh-agent command output

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-eval $(ssh-agent -s)
+eval $(ssh-agent -s) > /dev/null
 
 [ -z "${SSH_PRIVATE_KEY}" ] || echo "${SSH_PRIVATE_KEY}" | tr -d '\r' | ssh-add -
 [ -z "${GIT_USER_NAME}" ] || git config --global user.name "${GIT_USER_NAME}"

151c2e4426eb8692a73eea8fd56feddad17fe40e
Walter Gomez <47392003+ictus4u@users.noreply.github.com>
2021-03-30 17:21:56 -0400

    Make optional to set the expand_aliases flag

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,10 +3,9 @@ set -e
 
 eval $(ssh-agent -s) > /dev/null
 
-[ -z "${SSH_PRIVATE_KEY}" ] || echo "${SSH_PRIVATE_KEY}" | tr -d '\r' | ssh-add -
-[ -z "${GIT_USER_NAME}" ] || git config --global user.name "${GIT_USER_NAME}"
-[ -z "${GIT_USER_EMAIL}" ] || git config --global user.email "${GIT_USER_EMAIL}"
-
-shopt -s expand_aliases
+[ -z "${SSH_PRIVATE_KEY}"      ] || echo "${SSH_PRIVATE_KEY}" | tr -d '\r' | ssh-add -
+[ -z "${GIT_USER_NAME}"        ] || git config --global user.name "${GIT_USER_NAME}"
+[ -z "${GIT_USER_EMAIL}"       ] || git config --global user.email "${GIT_USER_EMAIL}"
+[ -z "${SHELL_EXPAND_ALIASES}" ] || gshopt -s expand_aliases
 
 exec "$@"

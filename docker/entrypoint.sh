4bf6bcfe00c4dc724e980c0eda5cad04f415b458
Ictus <ictus4u@gmail.com>
2023-04-23 04:13:56 +0200

    Add quotation marks

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-eval $(ssh-agent -s) > /dev/null
+eval "$(ssh-agent -s)" > /dev/null
 
 [ -z "${SSH_PRIVATE_KEY}"      ] || echo "${SSH_PRIVATE_KEY}" | tr -d '\r' | ssh-add -
 [ -z "${GIT_USER_NAME}"        ] || git config --global user.name "${GIT_USER_NAME}"
 [ -z "${GIT_USER_EMAIL}"       ] || git config --global user.email "${GIT_USER_EMAIL}"
 [ -z "${SHELL_EXPAND_ALIASES}" ] || shopt -s expand_aliases
 
-exec "$@"
+exec "${@}"

ca9d01b2ea68ec9868d5d9a31a180fa7e42f5dc2
Ictus <ictus4u@gmail.com>
2023-04-26 09:26:14 +0200

    Parse SHELL_EXPAND_ALIASES for a truthy value

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
+parse_true() {
+    [ -n "${1:-}" ] || return 1
+    case "${1,,}" in
+        1|true|t|yes|y|on)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
 eval "$(ssh-agent -s)" > /dev/null
 
 [ -z "${SSH_PRIVATE_KEY}"      ] || echo "${SSH_PRIVATE_KEY}" | tr -d '\r' | ssh-add -
 [ -z "${GIT_USER_NAME}"        ] || git config --global user.name "${GIT_USER_NAME}"
 [ -z "${GIT_USER_EMAIL}"       ] || git config --global user.email "${GIT_USER_EMAIL}"
-[ -z "${SHELL_EXPAND_ALIASES}" ] || shopt -s expand_aliases
+! parse_true "${SHELL_EXPAND_ALIASES}" || shopt -s expand_aliases
 
 exec "${@}"
